c771395bef38ac84bc3085e80216cb6b96bfb00b
JonathanM307 <jomanriquego@uide.edu.ec>
2026-06-25 23:30:28 -0500

    definir limites de busqueda e interactividad basica por consola

---
+++ ProyectoFinal.py
@@ -0,0 +1,13 @@
+# Versión 1: Inicialización del Entorno
+
+print("============================================================")
+print("  SISTEMA INTEGRADOR: ALGORITMO ADIVINA EL NÚMERO (V1.0)")
+print("============================================================")
+
+# Definición del rango inicial de búsqueda
+minimo = 1
+maximo = 100
+intentos = 0
+
+print(f"Piensa en un número secreto entre {minimo} y {maximo}.")
+input("Presiona Enter cuando estés listo para comenzar...")
