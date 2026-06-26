11fbd2c041f80eec8c58c62d9122c2299ce82d29
JonathanM307 <jomanriquego@uide.edu.ec>
2026-06-25 23:32:13 -0500

    incorporar condicionales if-elif para modificacion de limites

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -1,4 +1,4 @@
-# Versión 3: Ciclo Iterativo Principal
+# Versión 4: Ajuste de Límites Algorítmicos
 
 minimo = 1
 maximo = 100
@@ -7,12 +7,17 @@ respuesta = ""
 
 print("Piensa en un número secreto entre 1 y 100...")
 
-# Bucle interactivo controlado por la respuesta de éxito
 while respuesta != "correcto":
     intento = (minimo + maximo) // 2
     intentos += 1
     
     print(f"\n[Intento No. {intentos}] ¿Tu número secreto es el {intento}?")
     respuesta = input("Respuesta (mayor / menor / correcto): ").strip()
+    
+    # Estructura condicional compuesta para redefinir el espacio matemático
+    if respuesta == "mayor":
+        minimo = intento + 1
+    elif respuesta == "menor":
+        maximo = intento - 1
 
-print(f"\n[Éxito] El computador ha finalizado el ciclo en {intentos} intentos.")
+print(f"\n¡Logrado! El número ha sido identificado tras {intentos} intentos.")
